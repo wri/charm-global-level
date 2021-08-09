7b3f4ded56c611962b339262529dc96af8b73386
Liqing.Peng <9502712+pitcheverlasting@users.noreply.github.com>
2021-08-09 18:13:40 -0400

    Add the wood fuel vs IND plot function

--- src/models/results_summary_analysis.py
+++ src/models/results_summary_analysis.py
@@ -14,7 +14,11 @@ import matplotlib.pyplot as plt
 root = '../../'
 discount_filename = '4p'
 datafile = '{}/data/processed/CHARM regional - DR_{} - Jul 22.xlsx'.format(root, discount_filename)
+# datafile = '{}/data/processed/CHARM regional - BAU - SF_1.2 - DR_4p - VSLP-IND - Jul 1.xlsx'.format(root)
 figdir = '{}/../Paper/Publication/Figure'.format(root)
+
+# FIXME WFL vs IND, I need to consider breakdown the plantation into the two, don't exclude plantation
+# FIXME run the all discounting rate scenario
 
 #############################################Read in model inputs###########################################
 ### Default parameters
@@ -139,28 +143,44 @@ def run_new_tropical_plantations_scenario(results):
     updated_secondary_area = (area_harvested_new_secondary_sum - area_reduced_secondary) / 1000000
     new_plantation_area = sum(area_harvested_new_agriland) / 1000000
 
+    # TEST print
+    # print('pdv_average_secondary_regrowth', pdv_average_secondary_regrowth)
+    # print('yield_average_secondary', yield_average_secondary)
+    # print('output_ha_tropical_average', output_ha_tropical_average)
+    # print('wood_supply_agriland', wood_supply_agriland)
+    # print('area_reduced_secondary', area_reduced_secondary)
+    # print('total_pdv_secondary_after_replace', total_pdv_secondary_after_replace)
+    # print('total_pdv_tropical_weighted_average', total_pdv_tropical_weighted_average)
+    # print('total_pdv_secondary_after_replace', total_pdv_secondary_after_replace)
+    # print('total_pdv_plantation', total_pdv_plantation)
+    # print('global_pdv', global_pdv)
+    # print('carbon_cost_annual', carbon_cost_annual)
+    # print('updated_secondary_area', updated_secondary_area)
+
     return carbon_cost_annual, updated_secondary_area, new_plantation_area
 
 
 def export_results_to_excel():
-    carbon_costs = np.zeros((8, 6))
-    required_area = np.zeros((8, 8))
+    carbon_costs = np.zeros((12, 6))
+    required_area = np.zeros((12, 8))
     row = 0
     row_index = []
-    for vslp_input_control in ['ALL', 'IND']:
+    for vslp_input_control in ['ALL', 'IND', 'WFL']:
         for substitution_mode in ['NOSUB', 'SUBON']:
             for future_demand_level in ['BAU', 'CST']:
                 output_tabname = '{}_{}_{}'.format(future_demand_level, substitution_mode, vslp_input_control)
                 row_index.append(output_tabname)
                 results = pd.read_excel(datafile, sheet_name=output_tabname)
                 carbon_main, land_main = extract_global_outputs_summary(results)
-
                 carbon_last, secondary_land_last, new_tropical_land_last = run_new_tropical_plantations_scenario(results)
-                carbon_costs[row, :5] = carbon_main.values
-                carbon_costs[row, 5] = carbon_last
-                required_area[row, :5] = land_main.values[1:]
-                # The 6th scenario
-                required_area[row, 5] = secondary_land_last
+                carbon_costs[row, :3] = carbon_main.values[:3]
+                carbon_costs[row, 4:6] = carbon_main.values[3:]
+                required_area[row, :3] = land_main.values[1:4]
+                required_area[row, 4:6] = land_main.values[4:]
+                # The 4th scenario new tropical plantation
+                carbon_costs[row, 3] = carbon_last
+                required_area[row, 3] = secondary_land_last
+
                 required_area[row, 6] = land_main.values[0]  # The existing plantation
                 required_area[row, 7] = new_tropical_land_last  # The new plantation
 
@@ -168,7 +188,7 @@ def export_results_to_excel():
 
     # Convert to pandas dataframe
     scenarios = ['S1 Secondary forest harvest + regrowth', 'S2 Secondary forest harvest + conversion', 'S3 Secondary forest mixed harvest',
-     'S4 Higher plantation productivity', 'S5 Less wood fuel 2050 demand', 'S6 New tropical plantations']
+     'S4 New tropical plantations', 'S5 Higher plantation productivity', 'S6 50% less 2050 wood fuel demand']
     land_areas = scenarios + ['Existing plantations', 'New plantations']
     carbon_df = pd.DataFrame(carbon_costs, index=row_index, columns=scenarios)
     area_df = pd.DataFrame(required_area, index=row_index, columns=land_areas)
@@ -209,6 +229,7 @@ def barplot_all_scenarios():
     def stacked_barplot_attribute_demand_substitution_carbon(result_df, title, ylabel):
         "Plot multiple scenarios"
         fig, ax = plt.subplots(1, figsize=(14, 8))
+        result_df[result_df.select_dtypes(include=['number']).columns] *= -1
         plt.bar(result_df.columns, result_df.loc['CST_NOSUB_ALL'], color='#DB4444', width=0.5)
         plt.bar(result_df.columns, result_df.loc['NewDemand_NOSUB_ALL'], bottom=result_df.loc['CST_NOSUB_ALL'], color='#E17979', width=0.5)
         plt.bar(result_df.columns, result_df.loc['BAU_SubEffect_ALL'], color='#337AE3', width=0.5)
@@ -221,13 +242,13 @@ def barplot_all_scenarios():
         ax.spines['right'].set_visible(False)
         ax.spines['left'].set_visible(False)
         ax.spines['top'].set_visible(False)
-        ax.spines['bottom'].set_visible(False)
+        # ax.spines['bottom'].set_visible(False)
         # grid
         ax.set_axisbelow(True)
         ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.7)
         # x ticks
         xticks_labels = ['S1 Secondary forest\nHarvest + Regrowth', 'S2 Secondary forest\nHarvest + Conversion', 'S3 Secondary forest\nMixed harvest',
-     'S4 Higher\nPlantation productivity', 'S5 Less wood fuel\n2050 Demand', 'S6 New\nTropical plantations']
+                         'S4 New\nTropical plantations', 'S5 Higher\nPlantation productivity', 'S6 50% less 2050\nWood fuel demand']
         plt.xticks(result_df.columns, labels=xticks_labels)
         # bar labels
         base = result_df.loc['BAU_NOSUB_ALL']
@@ -238,6 +259,13 @@ def barplot_all_scenarios():
             ax.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2,
                     "{:.0f}%".format(height/base[bar_scenario_group]*100),
                       ha='center', va='bottom')
+
+        for number, rec in enumerate(ax.patches[18:]):
+            height = rec.get_height()
+            # This is to make sure the turn of the group of scenario
+            ax.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height,
+                    "{:.1f}".format(height),
+                      ha='center', va='bottom', fontweight='bold')
 
         # title and legend
         legend_label = ['2010 supply level', 'Additional BAU demand', 'Substitution benefit', 'Net carbon impact']
@@ -253,7 +281,7 @@ def barplot_all_scenarios():
         result_df_secondary = result_df.iloc[:, range(6)]
         result_df_secondary.loc['Existing plantations'] = result_df.loc['BAU_NOSUB_ALL', 'Existing plantations']
         result_df_secondary.loc['New plantations'] = 0
-        result_df_secondary.loc['New plantations', 'S6 New tropical plantations'] = result_df.loc['BAU_NOSUB_ALL', 'New plantations']
+        result_df_secondary.loc['New plantations', 'S4 New tropical plantations'] = result_df.loc['BAU_NOSUB_ALL', 'New plantations']
 
         fig, ax = plt.subplots(1, figsize=(14, 8))
         plt.bar(result_df_secondary.columns, result_df_secondary.loc['Existing plantations'], color='#3776ab', edgecolor='k', width=0.5)
@@ -269,13 +297,13 @@ def barplot_all_scenarios():
         ax.spines['right'].set_visible(False)
         ax.spines['left'].set_visible(False)
         ax.spines['top'].set_visible(False)
-        ax.spines['bottom'].set_visible(False)
+        # ax.spines['bottom'].set_visible(False)
         # grid
         ax.set_axisbelow(True)
         ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.7)
         # x ticks
         xticks_labels = ['S1 Secondary forest\nHarvest + Regrowth', 'S2 Secondary forest\nHarvest + Conversion', 'S3 Secondary forest\nMixed harvest',
-     'S4 Higher\nPlantation productivity', 'S5 Less wood fuel\n2050 Demand', 'S6 New\nTropical plantations']
+     'S4 New\nTropical plantations', 'S5 Higher\nPlantation productivity', 'S6 50% less 2050\nWood fuel demand']
         plt.xticks(result_df_secondary.columns, labels=xticks_labels)
         # bar labels
         base = result_df_secondary.loc['BAU_NOSUB_ALL'] + result_df_secondary.loc['Existing plantations'] + result_df_secondary.loc['New plantations']
@@ -286,10 +314,17 @@ def barplot_all_scenarios():
             ax.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2,
                     "{:.0f}%".format(height/base[bar_scenario_group]*100),
                       ha='center', va='bottom')
-        # Add the sixth scenario's new tropical plantations percentage
-        ax.text(ax.patches[23].get_x() + ax.patches[23].get_width() / 2, ax.patches[23].get_y() + ax.patches[23].get_height()/2,
-                "{:.0f}%".format(ax.patches[23].get_height() / base[5] * 100),
+        # Add the fourth scenario's new tropical plantations percentage
+        ax.text(ax.patches[21].get_x() + ax.patches[21].get_width() / 2, ax.patches[21].get_y() + ax.patches[21].get_height()/2,
+                "{:.0f}%".format(ax.patches[21].get_height() / base[3] * 100),
                 ha='center', va='bottom')
+        # Add the final absolute numbers at the top
+        area_total = result_df_secondary.loc['Existing plantations'] + result_df_secondary.loc['CST_NOSUB_ALL'] + result_df_secondary.loc['NewDemand_NOSUB_ALL'] + result_df_secondary.loc['New plantations']
+        for number, rec in enumerate(ax.patches[:6]):
+            # This is to make sure the turn of the group of scenario
+            ax.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + area_total[number],
+                    "{:.0f}".format(area_total[number]),
+                      ha='center', va='bottom', fontweight='bold')
 
         # title and legend
         legend_label = ['Existing plantations', '2010 supply level', 'Additional BAU demand', 'New tropical plantations', 'Total secondary forest area']
@@ -303,12 +338,126 @@ def barplot_all_scenarios():
 
         return
 
+    def stacked_barplot_attribute_IND_WFL_carbon(result_df, title, ylabel):
+        "Plot multiple scenarios"
+        fig, ax = plt.subplots(1, figsize=(14, 8))
+        result_df[result_df.select_dtypes(include=['number']).columns] *= -1
+        plt.bar(result_df.columns, result_df.loc['BAU_SUBON_ALL']*result_df.loc['BAU_SUBON_IND']/(result_df.loc['BAU_SUBON_IND']+result_df.loc['BAU_SUBON_WFL']), color='#ab5b1a', width=0.5)
+        plt.bar(result_df.columns, result_df.loc['BAU_SUBON_ALL']*result_df.loc['BAU_SUBON_WFL']/(result_df.loc['BAU_SUBON_IND']+result_df.loc['BAU_SUBON_WFL']), bottom=result_df.loc['BAU_SUBON_ALL']*result_df.loc['BAU_SUBON_IND']/(result_df.loc['BAU_SUBON_IND']+result_df.loc['BAU_SUBON_WFL']), color='#d48f57', width=0.5)
+        plt.bar(result_df.columns, result_df.loc['BAU_SUBON_ALL'], ls='dashed', facecolor="None", edgecolor='k', width=0.5)
+        # x and y limits
+        # plt.xlim(-0.6, 10.5)
+        # plt.ylim(-1600, 2000)
+        plt.ylabel(ylabel, fontsize=14)
+        # remove spines
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        # ax.spines['bottom'].set_visible(False)
+        # grid
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.7)
+        # x ticks
+        xticks_labels = ['S1 Secondary forest\nHarvest + Regrowth', 'S2 Secondary forest\nHarvest + Conversion', 'S3 Secondary forest\nMixed harvest',
+     'S4 Higher\nPlantation productivity', 'S5 50% Less wood fuel\n2050 Demand', 'S6 New\nTropical plantations']
+        plt.xticks(result_df.columns, labels=xticks_labels)
+        # bar labels
+        base = result_df.loc['BAU_SUBON_ALL']
+        for number, rec in enumerate(ax.patches[:12]):
+            height = rec.get_height()
+            # This is to make sure the turn of the group of scenario
+            bar_scenario_group = number%6
+            ax.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2,
+                    "{:.0f}%".format(height/base[bar_scenario_group]*100),
+                      ha='center', va='bottom')
+
+        for number, rec in enumerate(ax.patches[12:]):
+            height = rec.get_height()
+            # This is to make sure the turn of the group of scenario
+            ax.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height,
+                    "{:.1f}".format(height),
+                      ha='center', va='bottom', fontweight='bold')
+
+        # title and legend
+        legend_label = ['Industrial roundwood', 'Wood fuel', 'Total carbon impact']
+        plt.legend(legend_label, ncol=4, bbox_to_anchor=([1, 1.05, 0, 0]), frameon=False, fontsize=13)
+        plt.title('{}\n'.format(title), loc='left', fontsize=16)
+        # plt.show()
+        plt.savefig('{}/carbon_cost_annual_IND_WFL_6scenarios.png'.format(figdir))
+
+        return
+
+    def stacked_barplot_attribute_IND_WFL_land(result_df, title, ylabel):
+        "Plot multiple scenarios"
+        result_df_secondary = result_df.iloc[:, range(6)]
+        result_df_secondary.loc['Existing plantations'] = result_df.loc['BAU_NOSUB_ALL', 'Existing plantations']
+        result_df_secondary.loc['New plantations'] = 0
+        result_df_secondary.loc['New plantations', 'S4 New tropical plantations'] = result_df.loc['BAU_NOSUB_ALL', 'New plantations']
+
+        fig, ax = plt.subplots(1, figsize=(14, 8))
+        plt.bar(result_df_secondary.columns, result_df_secondary.loc['Existing plantations'], color='#7c3d16', edgecolor='k', width=0.5)
+        plt.bar(result_df_secondary.columns, result_df_secondary.loc['BAU_SUBON_ALL']*result_df_secondary.loc['BAU_SUBON_IND']/(result_df_secondary.loc['BAU_SUBON_IND']+result_df_secondary.loc['BAU_SUBON_WFL']), bottom=result_df_secondary.loc['Existing plantations'], color='#ab5b1a', width=0.5)
+        plt.bar(result_df_secondary.columns, result_df_secondary.loc['BAU_SUBON_ALL']*result_df_secondary.loc['BAU_SUBON_WFL']/(result_df_secondary.loc['BAU_SUBON_IND']+result_df_secondary.loc['BAU_SUBON_WFL']), bottom=result_df_secondary.loc['Existing plantations']+result_df_secondary.loc['BAU_SUBON_ALL']*result_df_secondary.loc['BAU_SUBON_IND']/(result_df_secondary.loc['BAU_SUBON_IND']+result_df_secondary.loc['BAU_SUBON_WFL']), color='#d48f57', width=0.5)
+        plt.bar(result_df_secondary.columns, result_df_secondary.loc['New plantations'], bottom=result_df_secondary.loc['Existing plantations']+result_df_secondary.loc['BAU_SUBON_ALL'], edgecolor='k', color='#f59e1b', width=0.5)
+        plt.bar(result_df_secondary.columns, result_df_secondary.loc['BAU_SUBON_ALL'], bottom=result_df_secondary.loc['Existing plantations'], ls='dashed', facecolor="None", edgecolor='k', width=0.5)
+        # x and y limits
+        # plt.xlim(-0.6, 10.5)
+        # plt.ylim(-1600, 2000)
+        plt.ylabel(ylabel, fontsize=14)
+        # remove spines
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        # ax.spines['bottom'].set_visible(False)
+        # grid
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.7)
+        # x ticks
+        xticks_labels = ['S1 Secondary forest\nHarvest + Regrowth', 'S2 Secondary forest\nHarvest + Conversion', 'S3 Secondary forest\nMixed harvest',
+     'S4 Higher\nPlantation productivity', 'S5 50% less wood fuel\n2050 Demand', 'S6 New\nTropical plantations']
+        plt.xticks(result_df_secondary.columns, labels=xticks_labels)
+        # bar labels
+        base = result_df_secondary.loc['BAU_NOSUB_ALL'] + result_df_secondary.loc['Existing plantations'] + result_df_secondary.loc['New plantations']
+        for number, rec in enumerate(ax.patches[:18]):
+            height = rec.get_height()
+            # This is to make sure the turn of the group of scenario
+            bar_scenario_group = number%6
+            ax.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2,
+                    "{:.0f}%".format(height/base[bar_scenario_group]*100),
+                      ha='center', va='bottom')
+        # Add the fourth scenario's new tropical plantations percentage
+        ax.text(ax.patches[21].get_x() + ax.patches[21].get_width() / 2, ax.patches[21].get_y() + ax.patches[21].get_height() / 2,
+                "{:.0f}%".format(ax.patches[21].get_height() / base[3] * 100),
+                ha='center', va='bottom')
+        # Add the final absolute numbers at the top
+        area_total = result_df_secondary.loc['Existing plantations'] + result_df_secondary.loc['BAU_SUBON_ALL'] + result_df_secondary.loc['New plantations']
+        for number, rec in enumerate(ax.patches[:6]):
+            # This is to make sure the turn of the group of scenario
+            ax.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + area_total[number],
+                    "{:.0f}".format(area_total[number]),
+                      ha='center', va='bottom', fontweight='bold')
+
+        # title and legend
+        legend_label = ['Existing plantations', 'Industrial roundwood', 'Wood fuel', 'New tropical plantations', 'Total secondary forest area']
+        plt.legend(legend_label, ncol=2, bbox_to_anchor=([1, 1.15, 0, 0]), frameon=False, fontsize=14)
+
+        # sort both labels and handles by labels
+        plt.subplots_adjust(top=0.83)
+        plt.title('{}\n'.format(title), loc='left', fontsize=16, y=1.08)
+        plt.savefig('{}/land_requirement_IND_WFL_6scenarios.png'.format(figdir))
+        # plt.show()
+
+        return
+
+
     carbon_df = read_dataframe('CO2 (Gt per yr) DR_{}'.format(discount_filename))
-    stacked_barplot_attribute_demand_substitution_carbon(carbon_df, 'Carbon impact', 'GtCO2/year')
+    stacked_barplot_attribute_demand_substitution_carbon(carbon_df, 'Carbon costs', 'GtCO2/year')
+    # stacked_barplot_attribute_IND_WFL_carbon(carbon_df, 'Carbon costs', 'GtCO2/year')
 
     # land_df = read_dataframe('Land (Mha) DR_{}'.format(discount_filename))
-    # stacked_barplot_attribute_demand_substitution_land(land_df, 'Land requirement 2010-2050', 'Mha')
+    # stacked_barplot_attribute_demand_substitution_land(land_df, 'Land requirements 2010-2050', 'Mha')
+    # stacked_barplot_attribute_IND_WFL_land(land_df, 'Land requirements 2010-2050', 'Mha')
 
     return
 
-# barplot_all_scenarios()
+barplot_all_scenarios()
