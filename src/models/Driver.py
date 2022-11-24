d3d0cc3860a0aa80cea20a66a162e7236426f3fb
Liqing.Peng <9502712+pitcheverlasting@users.noreply.github.com>
2022-11-23 23:48:28 -0500

    Create a run function only for the scenario 1

--- src/models/Driver.py
+++ src/models/Driver.py
@@ -489,11 +489,186 @@ def run_model_all_scenarios(discount_rate, years_filename):
     return
 
 
+def run_model_main_scenario(discount_rate, years_filename):
+    """
+    Created and Edited: 2022/11
+    This is a driver for running global analysis for forestry land and carbon consequences.
+    This is only for the main regrowth scenario, to save running time for sensitivity analysis
+    """
+    # Read input/output data excel file.
+    # datafile = '{}/data/processed/CHARM regional - DR_{} - {} - Feb 10 2022.xlsx'.format(root, discount_rate, years_filename)
+    # sensitivity analysis
+    growth_exps = ['GR 25U', 'GR 25D', 'GR2_GR1 25D', 'GR2_GR1 25U', 'GR2_GR1 50D']
+    rootshoot_exps = ['RSR 25U', 'RSR 25D']
+    trade_exps = ['trade 50U', 'trade 50D']
+    demand_exps = ['demand OECD', 'demand IIASA', 'demand LINE']
+    experiment = demand_exps[2]
+    datafile = '{}/data/processed/CHARM regional - DR_{} - {} - Nov 1 2022 - {}.xlsx'.format(root, discount_rate, years_filename, experiment)
+
+    # Read in countries
+    countries = pd.read_excel(datafile, sheet_name='Inputs', usecols="A:B", skiprows=1)
+    # Read in input data
+    input_data = pd.read_excel(datafile, sheet_name='Inputs', skiprows=1)
+    # If the cell has formula, it will be read as NaN
+
+    def single_run_with_combination_input(future_demand_level_input='BAU', substitution_mode_input='SUBON', vslp_input_control_input='ALL'):
+        """
+        This function generates the 30 countries results for each scenario, depending on the combination of below parameters.
+        :param future_demand_level_input: select future demand level from "BAU" business-as-usual or "CST": constant demand
+        :param substitution_mode_input: select substitution mode from "SUBON" with substitution or "NOSUB" gross carbon impacts
+        :param vslp_input_control_input: select VSLP option from "ALL" total roundwood (VSLP_WFL+VSLP_IND) or "IND" industrial roundwood (VSLP_IND)
+        :return:
+        """
+        # Initiate the output variables.
+        # country name, code
+        countrynames, codes = [], []
+        # wood supply
+        secondary_wood_default, plantation_wood_default = [], []
+        # pdv
+        pdv_per_ha_plantation_default = []
+        pdv_per_ha_regrowth = []
+        # area
+        area_plantation = []
+        area_regrowth, carbon_regrowth, carbon_existing_plantation, carbon_secondary_regrowth = [], [], [], []
+        output_ha_agriland = []
+
+        # For each country, calculate the results for all scenarios. Each variable collects a list of the countries' results.
+        for country, code in zip(countries['Country'], countries['ISO']):
+            # Test if the parameters are set up for this country, if there is one parameter missing, no calculation will be done for this country.
+            input_country = input_data.loc[input_data['Country'] == country]
+            input_country = input_country.drop(['Emissions substitution factor for LLP (tC saved/tons C in LLP)'], axis=1)
+            if input_country.isnull().values.any():
+                print("Please fill in the abbreviation and all the missing parameters for country '{}'!".format(country))
+            else:
+                ################################### Execute model runs ##################################
+                nyears_harvest_settings = Global_by_country.SetupTime(datafile, country_iso=code, nyears_run_control='harvest')
+                nyears_growth_settings = Global_by_country.SetupTime(datafile, country_iso=code, nyears_run_control='growth')
+                ### Default plantation scenarios, (1) secondary harvest regrowth and (2) conversion
+                ### Read in global parameters ###
+                global_harvest_settings = Global_by_country.Parameters(datafile, nyears_harvest_settings,
+                                                                        country_iso=code,
+                                                                       future_demand_level=future_demand_level_input,
+                                                                       substitution_mode=substitution_mode_input,
+                                                                       vslp_input_control=vslp_input_control_input)
+                global_growth_settings = Global_by_country.Parameters(datafile, nyears_growth_settings,
+                                                                       country_iso=code,
+                                                                       future_demand_level=future_demand_level_input,
+                                                                       substitution_mode=substitution_mode_input,
+                                                                       vslp_input_control=vslp_input_control_input)
+                # run different policy scenarios
+                result_plantation_default = Plantation_counterfactual_secondary_plantation_age_scenario.CarbonTracker(global_harvest_settings)
+                # result_conversion_default = Secondary_conversion_scenario.CarbonTracker(global_harvest_settings)
+                result_regrowth_default = Secondary_regrowth_scenario.CarbonTracker(global_harvest_settings)
+                # result_agriland_default = Agricultural_land_tropical_scenario.CarbonTracker(global_harvest_settings)
+
+                # run the land area calculator
+                LAC_default = Land_area_calculator.LandCalculator(global_harvest_settings)
+                if global_harvest_settings.rotation_length_harvest == 10:
+                    output_ha_agriland_default = LAC_default.output_ha_agriland[global_harvest_settings.year_index_harvest_plantation[1]-1]
+                else:
+                    output_ha_agriland_default = 0
+                # run the carbon cost calculator
+                CCC_default = Carbon_cost_calculator.CarbonCalculator(global_harvest_settings, global_growth_settings, LAC_default)
+
+                ################################### Prepare output ##################################
+                countrynames.append(country)
+                codes.append(code)
+
+                # Carbon tracker
+                pdv_per_ha_plantation_default.append(np.sum(result_plantation_default.annual_discounted_value))
+                pdv_per_ha_regrowth.append(np.sum(result_regrowth_default.annual_discounted_value))
+
+                # Get output per ha for new plantation
+                output_ha_agriland.append(output_ha_agriland_default)
+
+                # Default situation
+                secondary_wood_default.append(sum(LAC_default.output_need_secondary / 1000000))
+                plantation_wood_default.append(sum(LAC_default.product_total_carbon) / 1000000 - sum(LAC_default.output_need_secondary) / 1000000)
+                area_plantation.append(sum(CCC_default.area_harvested_new_plantation))
+
+                # Scenario 1
+                area_regrowth.append(sum(LAC_default.area_harvested_new_secondary_regrowth_combined))
+                carbon_regrowth.append(CCC_default.total_pdv_plantation_secondary_regrowth)
+                carbon_existing_plantation.append(CCC_default.total_pdv_plantation_sum)
+                carbon_secondary_regrowth.append(CCC_default.total_pdv_secondary_regrowth_sum)
+
+
+        # Save to the excel file
+        dataframe = pd.DataFrame({'Country': countrynames,
+                                  'ISO': codes,
+                                  # Save PDV
+                                  'PDV per ha Secondary middle regrowth (tC/ha)': pdv_per_ha_regrowth,
+                                  'PDV per ha Plantation (tC/ha)': pdv_per_ha_plantation_default,
+                                  # Save output
+                                  'Output per ha Agricultural land conversion (tC/ha)': output_ha_agriland,
+                                  # Save wood supply
+                                  'Default: Plantation supply wood (mega tC)': plantation_wood_default,
+                                  'Default: Secondary forest supply wood (mega tC)': secondary_wood_default,
+
+                                  # Save plantation area
+                                  'Plantation area (ha)': area_plantation,
+
+                                  # S1
+                                  'S1 regrowth: Secondary area (ha)': area_regrowth,
+                                  'S1 regrowth: total PDV (mega tC)': carbon_regrowth,
+                                  'S1 regrowth: PDV plantation (mega tC)': carbon_existing_plantation,
+                                  'S1 regrowth: PDV secondary (mega tC)': carbon_secondary_regrowth,
+
+                                  })
+
+        def write_excel(filename, sheetname, dataframe):
+            "This function will overwrite the Outputs sheet"
+            with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
+                workbook = writer.book
+                try:
+                    workbook.remove(workbook[sheetname])
+                    print("Updating Outputs sheet...")
+                except:
+                    print("Creating Outputs sheet...")
+                finally:
+                    dataframe.to_excel(writer, sheet_name=sheetname, index=False)
+                    writer.save()
+
+        # Prepare output tab name
+        output_tabname = '{}_{}_{}'.format(future_demand_level_input, substitution_mode_input, vslp_input_control_input)
+        write_excel(datafile, output_tabname, dataframe)
+
+
+    ################## Run the experiments ###################
+    def run_single_input(future_demand_level, substitution_mode, vslp_input_control):
+        "Run model with a set of parameters"
+        single_run_with_combination_input(future_demand_level_input=future_demand_level,
+                                          substitution_mode_input=substitution_mode,
+                                          vslp_input_control_input=vslp_input_control)
+        return
+
+    def run_all_input_permutations():
+        "Run model based on the different combination of the three parameters"
+        for vslp_input_control in ['ALL', 'IND', 'WFL']:
+            for substitution_mode in ['NOSUB', 'SUBON']:
+                for future_demand_level in ['BAU', 'CST']:
+                    single_run_with_combination_input(future_demand_level_input=future_demand_level, substitution_mode_input=substitution_mode, vslp_input_control_input=vslp_input_control)
+        return
+
+    def run_key_input_permutations():
+        "Run the key BAU and CST without substitution, only for sensitivity analysis"
+        for vslp_input_control in ['ALL']:
+            for substitution_mode in ['NOSUB']:
+                for future_demand_level in ['BAU', 'CST']:
+                    single_run_with_combination_input(future_demand_level_input=future_demand_level, substitution_mode_input=substitution_mode, vslp_input_control_input=vslp_input_control)
+        return
+
+    # run_all_input_permutations()
+    run_key_input_permutations()
+
+    return
+
 
 if __name__ == "__main__":
     # run_model_all_scenarios('4p', '40yr')
     # run_model_all_scenarios('0p', '100yr')
-    run_model_all_scenarios('4p', '40yr')
+    # run_model_all_scenarios('4p', '40yr')
+    run_model_main_scenario('4p', '40yr')
 
     exit()
 
