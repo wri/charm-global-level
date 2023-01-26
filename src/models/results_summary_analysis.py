9501abdacb033c933cecccb66123de4fcfe1791c
Liqing.Peng <9502712+pitcheverlasting@users.noreply.github.com>
2023-01-26 11:32:05 -0500

    1) split the new tropical area calculation to a standout module *prepare_new_tropical_plantations_area*, one can test it separately 2) add the fifth country ETH

--- src/models/results_summary_analysis.py
+++ src/models/results_summary_analysis.py
@@ -5,10 +5,10 @@
 3. Calculate the new tropical plantation scneario based on global results
 """
 __author__ = "Liqing Peng"
-__copyright__ = "Copyright (C) 2020-2021 World Resources Institute, The Carbon Harvest Model (CHARM) Project"
+__copyright__ = "Copyright (C) 2020-2023 World Resources Institute, The Carbon Harvest Model (CHARM) Project"
 __credits__ = ["Liqing Peng", "Jessica Zionts", "Tim Searchinger", "Richard Waite"]
 __license__ = "Polyform Strict License 1.0.0"
-__version__ = "2021.11.1"
+__version__ = "2023.1.1"
 __maintainer__ = "Liqing Peng"
 __email__ = "liqing.peng@wri.org"
 __status__ = "Dev"
@@ -23,25 +23,27 @@ root = '../..'
 
 discount_filename = '4p'
 years_filename = '40yr'
-# Sensitivity analysis
-growth_exps = ['GR 25U', 'GR 25D', 'GR2_GR1 25D', 'GR2_GR1 25U', 'GR2_GR1 50D']
-rootshoot_exps = ['RSR 25U', 'RSR 25D']
-trade_exps = ['trade 50U', 'trade 50D']
-demand_exps = ['demand OECD', 'demand IIASA', 'demand LINE']
-exp_filename = demand_exps[2]
 
-# datafile = '{}/data/processed/CHARM regional - DR_{} - Jan 11 2022.xlsx'.format(root, discount_filename)
-# datafile = '{}/data/processed/CHARM regional - DR_{} - {} - Feb 10 2022.xlsx'.format(root, discount_filename, years_filename)
-datafile = '{}/data/processed/CHARM regional - DR_{} - {} - Nov 1 2022 - {}.xlsx'.format(root, discount_filename, years_filename, exp_filename)
-outdir = '{}/data/processed/derivative/sensitivity_analysis'.format(root)
-# outdir = '{}/data/processed/derivative'.format(root)
+
+datafile = '{}/data/processed/CHARM regional - DR_{} - {} - Jan 18 2023.xlsx'.format(root, discount_filename, years_filename)
+outdir = '{}/data/processed/derivative'.format(root)
+
+# Sensitivity analysis
+# growth_exps = ['GR 25U', 'GR 25D', 'GR2_GR1 25D', 'GR2_GR1 25U', 'GR2_GR1 50D']
+# rootshoot_exps = ['RSR 25U', 'RSR 25D']
+# trade_exps = ['trade 50U', 'trade 50D']
+# demand_exps = ['demand OECD', 'demand IIASA', 'demand LINE']
+# exp_filename = demand_exps[2]
+#
+# datafile = '{}/data/processed/CHARM regional - DR_{} - {} - Nov 1 2022 - {}.xlsx'.format(root, discount_filename, years_filename, exp_filename)
+# outdir = '{}/data/processed/derivative/sensitivity_analysis'.format(root)
 
 #############################################Read in model inputs###########################################
 ### Default parameters
 nyears_harvest_settings = Global_by_country.SetupTime(datafile, country_iso='BRA', nyears_run_control='harvest')
 Global = Global_by_country.Parameters(datafile, nyears_harvest_settings, country_iso='BRA')
 nyears = Global.nyears
-rotation_length = Global.rotation_length_harvest # this must be ten years
+rotation_length = Global.rotation_length_harvest # this is the rotation length from Brazil, currently set to 7
 
 ##############################################Read in model outputs##########################################
 # 2021 Nov Lists
@@ -88,6 +90,42 @@ def prepare_global_outputs_for_new_tropical_scenario(results):
     return carbon_global, area_global
 
 
+def prepare_new_tropical_plantations_area(nyears, rotation_length, area_established_agriland_annual_global):
+    """
+        This is for the Tropical New Plantation Scenario, 4th scenario's input
+        Set up plantation establishment rate per year, time and total area for harvest
+    """
+    # The first established plantation is in year 0, 2010; the last established plantation is in one rotation ahead of the last year. nyears - 1 + 1 for the range().
+    year_new_plantation_st, year_new_plantation_ed = 0, nyears - rotation_length
+    # The first harvested year of the established plantation is in one rotation behind of the first established year; the last harvested year is in last year, nyears - 1
+    year_harvest_st, year_harvest_ed = year_new_plantation_st + rotation_length, nyears - 1
+
+    area_established_new_agriland = np.zeros(nyears) # This is the area establishing plantation starting from year 0
+    area_harvested_new_agriland = np.zeros(nyears)  # This is the new established area starting from the first rotation end
+    area_harvested_agriland = np.zeros(nyears)   # This is the total harvested area plantation for each year's established plantation
+
+    # For each year of new established plantations, accumulate the area,
+    # and then count the number of rotation for that year's plantation (there may be multiple harvests in the same area over the course)
+    for year in range(year_new_plantation_st, year_new_plantation_ed):
+        area_established_new_agriland[year] = area_established_agriland_annual_global
+        # divide the rotation length and round down
+        nrotation = (year_harvest_ed - year) // rotation_length
+        area_harvested_agriland[year] = area_established_agriland_annual_global * nrotation
+
+    # after one rotation period
+    area_harvested_new_agriland[rotation_length:] = area_established_agriland_annual_global
+
+    # area_established_agriland = np.cumsum(area_established_new_agriland) # This is to show the accumulate agricultural land
+    area_established_new_agriland_accumulate = np.sum(area_established_new_agriland)
+    area_harvested_agriland_accumulate = np.sum(area_harvested_agriland)
+    area_harvested_new_agriland_accumulate = np.sum(area_harvested_new_agriland)
+
+    # CHECK - area_established_new_agriland_accumulate should EQUAL area_harvested_new_agriland_accumulate
+    if area_harvested_new_agriland_accumulate != area_established_new_agriland_accumulate:
+        raise ValueError('The total new tropical plantation area does not match!')
+    return area_established_new_agriland_accumulate, area_harvested_agriland_accumulate, area_harvested_new_agriland
+
+
 def run_new_tropical_plantations_scenario(results):
     """
     VERY IMPORTANT FEATURE OF THIS SCRIPT!
@@ -97,13 +135,9 @@ def run_new_tropical_plantations_scenario(results):
     """
 
     ### New plantation land area assumption
-    # We assume the 60 Mha of agricultural land are harvested evenly from 2021-2050 (2Mha per year, allowing the first 10 years for growth)
-    area_harvested_agriland_annual_global = 2 * 1000000  # Mha per year
-
-    area_harvested_new_agriland = np.zeros((nyears))
-    # starting from 2021, after one rotation period
-    area_harvested_new_agriland[rotation_length+1:] = area_harvested_agriland_annual_global
-    area_harvested_agriland = np.cumsum(area_harvested_new_agriland) # This is to show the accumulate agricultural land
+    # We assume the *** 2Mha *** per year of agricultural land are converted to plantation evenly from 2010-2050
+    area_established_agriland_annual_global = 2 * 1000000  # Mha per year
+    area_established_new_agriland_accumulate, area_harvested_agriland_accumulate, area_harvested_new_agriland = prepare_new_tropical_plantations_area(nyears, rotation_length, area_established_agriland_annual_global)
 
     ### Read in the country sum parameters
     carbon_global, area_global = prepare_global_outputs_for_new_tropical_scenario(results)
@@ -121,26 +155,21 @@ def run_new_tropical_plantations_scenario(results):
     yield_average_secondary = total_wood_secondary / area_harvested_new_secondary_sum
 
     ################################ Aggregated/Weighted Parameters
-    # only tropical countries that are 10 years rotation period: Brazil, Congo, Indonesia, Vietnam
+    # 2023/Jan: Change the rotation year from exactly 10 years to all countries that are below 10 years
+    # only tropical countries that are shorter than 10 years rotation period: Brazil, Congo, Ethiopia, Indonesia, Vietnam (Add Ethiopia)
     ### 3. Calculate average tropical plantation wood harvest
     # weighted average of the output per ha for each harvest
-    tropical_countries_iso = ['BRA', 'COD', 'IDN', 'VNM']
+    tropical_countries_iso = ['BRA', 'COD', 'ETH', 'IDN', 'VNM']
     # Get the plantation area
     area_plantation_tropical = [results.loc[results['ISO']==iso]['Plantation area (ha)'].values[0] for iso in tropical_countries_iso]
     # Get the output per ha at the 2020 year
     output_ha_tropical = [results.loc[results['ISO']==iso]['Output per ha Agricultural land conversion (tC/ha)'].values[0] for iso in tropical_countries_iso]
     weighted_sum = sum([area_plantation_tropical[i] * output_ha_tropical[i] for i in range(len(output_ha_tropical))])
-    # Get the weighted average output per ha for the four countries
+    # Get the weighted average output per ha for the five countries
     output_ha_tropical_average = weighted_sum / sum(area_plantation_tropical)
 
     ### 4. Calculate the total wood harvest/production from the new plantation from agricultural land
-        # 2020-2030, 2Mha per year*10 years means that 20 Mha will be harvested in the first 10 years.
-        # 2030-2040, the hectares harvested in the previous decade will be harvested a second time (assuming a 10 year rotation) for a total of 20Mha harvested PLUS the first harvest of 2Mha of new hectares each year (20Mha new) for a total of 40 Mha harvested.
-        # 2040-2050, the previous 40 Mha are harvested for either a second or third time, and 20 Mha are harvested for the first time.
-        # in year 2050, 2Mha * 4 = 8Mha will be harvested
-    # As a stair theory, every time you harvest the plantation,
-    area_harvested_agriland_accumulate = area_harvested_agriland_annual_global * rotation_length + area_harvested_agriland_annual_global * rotation_length * 2 + area_harvested_agriland_annual_global * rotation_length * 3 + area_harvested_agriland_annual_global * 1 * 4
-    # New plantation wood production for 2020-2050, tC, from 60 Mha agricultural land with continuous harvest
+    # New plantation wood production for 2020-2050, tC, from the 2 Mha per year agricultural land with continuous harvest
     wood_supply_agriland = area_harvested_agriland_accumulate * output_ha_tropical_average
 
     ### 5. Calculate area reduced due to new plantation as “new plantation wood production/average yield for secondary”
@@ -180,31 +209,15 @@ def run_new_tropical_plantations_scenario(results):
     ### 8. Calculate global PDV as Total PDV for existing plantation + Total PDV for new plantation + New total PDV for secondary
     global_pdv = total_pdv_plantation + total_pdv_secondary_after_replace + total_pdv_tropical_weighted_average
 
-    # Convert tC to GtCO2 per year. FIXME. change the 40 to 41.
+    # Convert tC to GtCO2 per year.
     carbon_cost_annual = global_pdv / 1000000000 * 44 / 12 / nyears
     # Convert ha to Mha
     updated_secondary_area = (area_harvested_new_secondary_sum - area_reduced_secondary) / 1000000
-    new_plantation_area = sum(area_harvested_new_agriland) / 1000000
+    new_plantation_area = np.sum(area_harvested_new_agriland) / 1000000
 
     ### 9. Calculate the total secondary wood supply reduced from the new plantation
     total_wood_secondary_after_replace = total_wood_secondary - wood_supply_agriland
     total_wood_plantation_after_replace = total_wood_plantation + wood_supply_agriland
-
-    # TEST print
-    # print('pdv_average_secondary_regrowth', pdv_average_secondary_regrowth)
-    # print('total_wood_secondary', total_wood_secondary)
-    # print('area_harvested_new_secondary_sum', area_harvested_new_secondary_sum)
-    # print('yield_average_secondary', yield_average_secondary)
-    # print('output_ha_tropical_average', output_ha_tropical_average)
-    # print('wood_supply_agriland', wood_supply_agriland)
-    # print('area_reduced_secondary', area_reduced_secondary)
-    # print('total_pdv_secondary_after_replace', total_pdv_secondary_after_replace)
-    # print('total_pdv_tropical_weighted_average', total_pdv_tropical_weighted_average)
-    # print('total_pdv_secondary_after_replace', total_pdv_secondary_after_replace)
-    # print('total_pdv_plantation', total_pdv_plantation)
-    # print('global_pdv', global_pdv)
-    # print('carbon_cost_annual', carbon_cost_annual)
-    # print('updated_secondary_area', updated_secondary_area)
 
     return carbon_cost_annual, updated_secondary_area, new_plantation_area, total_wood_secondary_after_replace, total_wood_plantation_after_replace
 
@@ -226,7 +239,7 @@ def calculate_existing_tropical_plantations_numbers():
     ### Calculate average tropical plantation wood harvest
     # only tropical countries that are 10 years rotation period: Brazil, Congo, Indonesia, Vietnam
     # weighted average of the output per ha for each harvest
-    tropical_countries_iso = ['BRA', 'COD', 'IDN', 'VNM']
+    tropical_countries_iso = ['BRA', 'COD', 'ETH', 'IDN', 'VNM']
     # Get the plantation area
     area_plantation_tropical = [results.loc[results['ISO']==iso]['Plantation area (ha)'].values[0] for iso in tropical_countries_iso]
     # Get the output per ha at the 2020 year
@@ -296,9 +309,8 @@ def export_results_to_excel():
                 dataframe.to_excel(writer, sheet_name=sheetname)
                 writer.save()
 
-    # outfile = '{}/data/processed/derivative/CHARM_global_carbon_land_summary.xlsx'.format(root)
-    # outfile = '{}/data/processed/derivative/CHARM_global_carbon_land_summary - {}.xlsx'.format(root, years_filename)
-    outfile = '{}/CHARM_global_carbon_land_summary - {} - {}.xlsx'.format(outdir, years_filename, exp_filename)
+    outfile = '{}/data/processed/derivative/CHARM_global_carbon_land_summary - {}.xlsx'.format(root, years_filename)
+    # outfile = '{}/CHARM_global_carbon_land_summary - {} - {}.xlsx'.format(outdir, years_filename, exp_filename)
 
     # Convert to pandas dataframe
     scenarios = ['S1 Secondary forest harvest + regrowth', 'S2 Secondary forest harvest + conversion', 'S3 Secondary forest mixed harvest',
@@ -325,8 +337,8 @@ def export_results_to_excel():
 
     return
 
-# export_results_to_excel()
-# exit()
+export_results_to_excel()
+exit()
 
 def export_results_to_excel_ALL_NOSUB():
     # 2 demand and sub levels x (6+1) scenarios
@@ -404,8 +416,8 @@ def export_results_to_excel_ALL_NOSUB():
 
     return
 
-export_results_to_excel_ALL_NOSUB()
-exit()
+# export_results_to_excel_ALL_NOSUB()
+# exit()
 
 def collect_all_discount_rates_results():
     "For Appendix 3"
